24ab70053a8b39abf0d04586559444dd9f2dd06c
LB-Squishy <laurentleboulbar@gmail.com>
2023-04-18 16:49:21 +0200

    Etape 3 : Ajout des Bullet Points Automatiques

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,6 @@
-//GESTION DES FLECHES DU CARROUSEL
+//GESTION DES SLIDES DU CARROUSEL***************************************
 
-// Ecoute du clic fleche gauche
-const arrowLeft = document.querySelector(".arrow_left");
-arrowLeft.addEventListener("click", function() {
-	console.log("Clic gauche OK!");
-});
-
-//ecoute du clic fleche droite
-const arrowRight = document.querySelector(".arrow_right");
-arrowRight.addEventListener("click", function() {
-	console.log("Clic droit OK!");
-});
-
-//GESTION DES POINTS DU CARROUSEL
-
-//GESTION DES SLIDES DU CARROUSEL
-
+// tableau des slides
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -34,3 +19,36 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// comptage des slides
+let totalSlides = slides.length;
+// test de modif de quantité de slides (décommenter la ligne suivante)
+// totalSlides = 12;
+console.log(totalSlides + " slides dans le carrousel");
+
+//GESTION DES FLECHES DU CARROUSEL****************************************
+
+// Ecoute du clic fleche gauche
+const arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", function() {
+	console.log("Clic gauche OK!");
+});
+
+//ecoute du clic fleche droite
+const arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", function() {
+	console.log("Clic droit OK!");
+});
+
+//GESTION DES POINTS DU CARROUSEL******************************************
+
+//ajout des points en fonction du nombre de slides
+const dots = document.querySelector(".dots");
+for (let slide = 0; slide <= totalSlides ; slide++){
+	const addDot = document.createElement("div");
+	addDot.classList.add("dot");
+	dots.appendChild(addDot);
+	//++ ajouter un if/else en fonction du slide avec .dot_selected au lieu de dot
+};
+
+
