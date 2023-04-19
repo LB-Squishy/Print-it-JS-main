68c7d5c62736aac2fb5638f63286011f82139583
LB-Squishy <laurentleboulbar@gmail.com>
2023-04-19 16:22:57 +0200

    Etape 4-5 : Slide & tag au clic infini

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-//GESTION DES SLIDES DU CARROUSEL***************************************
-
-// tableau des slides
+// TABLEAU DES SLIDES******************************************************
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -21,34 +19,60 @@ const slides = [
 ]
 
 // comptage des slides
+
 let totalSlides = slides.length;
-// test de modif de quantité de slides (décommenter la ligne suivante)
-// totalSlides = 12;
 console.log(totalSlides + " slides dans le carrousel");
 
-//GESTION DES FLECHES DU CARROUSEL****************************************
+//DEPART DU CARROUSEL******************************************************
 
-// Ecoute du clic fleche gauche
+let currentSlide = 0;
+const actualImg = document.querySelector(".banner-img");
+const actualTag = document.querySelector("#banner p");
+console.log (currentSlide,actualImg,actualTag);
+
+//GESTION DES FLECHES DU CARROUSEL*****************************************
+
+// Ecoute du clic fleche gauche-précédent-condition retour derniere
+
 const arrowLeft = document.querySelector(".arrow_left");
+
 arrowLeft.addEventListener("click", function() {
 	console.log("Clic gauche OK!");
+	// Slide & Tag précédent avec condition de retour à la dernière
+	currentSlide--;
+	if (currentSlide < 0) {
+		currentSlide = totalSlides-1;
+	} 
+	actualImg.src = "./assets/images/slideshow/"+slides[currentSlide].image;
+	actualTag.innerHTML = slides[currentSlide].tagLine;
+	console.log (currentSlide,actualImg,actualTag);
 });
 
-//ecoute du clic fleche droite
+//ecoute du clic fleche droite-suivant-condition retour première
+
 const arrowRight = document.querySelector(".arrow_right");
+
 arrowRight.addEventListener("click", function() {
 	console.log("Clic droit OK!");
+	// Slide & Tag suivant avec condition de retour à la première
+	currentSlide++;
+	if (currentSlide >= totalSlides) {
+		currentSlide = 0;
+	} 
+	actualImg.src = "./assets/images/slideshow/"+slides[currentSlide].image;
+	actualTag.innerHTML = slides[currentSlide].tagLine;
+	console.log (currentSlide,actualImg,actualTag);
 });
 
 //GESTION DES POINTS DU CARROUSEL******************************************
 
 //ajout des points en fonction du nombre de slides
+
 const dots = document.querySelector(".dots");
+
 for (let slide = 0; slide <= totalSlides ; slide++){
 	const addDot = document.createElement("div");
 	addDot.classList.add("dot");
 	dots.appendChild(addDot);
-	//++ ajouter un if/else en fonction du slide avec .dot_selected au lieu de dot
+	//++ ajouter un if/else (où?) en fonction du slide avec .dot_selected au lieu de dot
 };
-
-
