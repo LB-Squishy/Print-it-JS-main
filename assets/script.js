85e9de1793083885cc2f31555a90d65cc9d6d084
LB-Squishy <laurentleboulbar@gmail.com>
2023-04-21 20:15:52 +0200

    Etape 5 : BulletPoint réactif et Refactorisation

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-// TABLEAU DES SLIDES******************************************************
+/*TABLEAU DES SLIDES******************************************************
+	pour slides avec image et tag
+*/
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -18,61 +20,57 @@ const slides = [
 	}
 ]
 
-// comptage des slides
+/*NOUVEAU CARROUSEL******************************************************
+	utilise "TABLEAU DES SLIDES" en entrée
+*/
 
-let totalSlides = slides.length;
-console.log(totalSlides + " slides dans le carrousel");
+function newCarrousel() {
+	let currentSlide = 0;
+	dotCreate(currentSlide); //si présence de dot, sinon passer en commentaire
+	function generateSlide() {
+		document.querySelector(".banner-img").src = "./assets/images/slideshow/"+slides[currentSlide].image;
+		document.querySelector("#banner p").innerHTML = slides[currentSlide].tagLine;
+		dotCreate(currentSlide); //si présence de dot, sinon passer en commentaire
+		console.log ("Vous êtes sur la slide " + currentSlide);
+	}
+	function goPreviousSlide() {
+		const previousSlide = document.querySelector(".arrow_left");
+		previousSlide.addEventListener("click", function() {
+			currentSlide--;
+			if (currentSlide < 0) {
+				currentSlide = slides.length-1;
+			} 
+			generateSlide();
+		});
+	}
+	function goNextSlide () {
+		const nextSlide = document.querySelector(".arrow_right");
+		nextSlide.addEventListener("click", function() {
+			currentSlide++;
+			if (currentSlide >= slides.length) {
+				currentSlide = 0;
+			} 
+			generateSlide();
+		});
+	}
+	goPreviousSlide();
+	goNextSlide();
+}
+newCarrousel();
 
-//DEPART DU CARROUSEL******************************************************
+/*BULLETS POINTS POUR CARROUSEL******************************************
+	AddOn de "NOUVEAU CARROUSEL"
+*/
 
-let currentSlide = 0;
-const actualImg = document.querySelector(".banner-img");
-const actualTag = document.querySelector("#banner p");
-console.log (currentSlide,actualImg,actualTag);
-
-//GESTION DES FLECHES DU CARROUSEL*****************************************
-
-// Ecoute du clic fleche gauche-précédent-condition retour derniere
-
-const arrowLeft = document.querySelector(".arrow_left");
-
-arrowLeft.addEventListener("click", function() {
-	console.log("Clic gauche OK!");
-	// Slide & Tag précédent avec condition de retour à la dernière
-	currentSlide--;
-	if (currentSlide < 0) {
-		currentSlide = totalSlides-1;
-	} 
-	actualImg.src = "./assets/images/slideshow/"+slides[currentSlide].image;
-	actualTag.innerHTML = slides[currentSlide].tagLine;
-	console.log (currentSlide,actualImg,actualTag);
-});
-
-//ecoute du clic fleche droite-suivant-condition retour première
-
-const arrowRight = document.querySelector(".arrow_right");
-
-arrowRight.addEventListener("click", function() {
-	console.log("Clic droit OK!");
-	// Slide & Tag suivant avec condition de retour à la première
-	currentSlide++;
-	if (currentSlide >= totalSlides) {
-		currentSlide = 0;
-	} 
-	actualImg.src = "./assets/images/slideshow/"+slides[currentSlide].image;
-	actualTag.innerHTML = slides[currentSlide].tagLine;
-	console.log (currentSlide,actualImg,actualTag);
-});
-
-//GESTION DES POINTS DU CARROUSEL******************************************
-
-//ajout des points en fonction du nombre de slides
-
-const dots = document.querySelector(".dots");
-
-for (let slide = 0; slide <= totalSlides ; slide++){
-	const addDot = document.createElement("div");
-	addDot.classList.add("dot");
-	dots.appendChild(addDot);
-	//++ ajouter un if/else (où?) en fonction du slide avec .dot_selected au lieu de dot
-};
+function dotCreate(currentSlide) {
+	const dots = document.querySelector(".dots");
+	dots.innerHTML= "";
+	for (let dotIndex = 0; dotIndex <= slides.length-1; dotIndex++){
+		const addDot = document.createElement("div");
+		addDot.classList.add("dot");		
+		if (dotIndex === currentSlide){
+			addDot.classList.add("dot_selected");
+		}
+		dots.appendChild(addDot);
+	}
+}
